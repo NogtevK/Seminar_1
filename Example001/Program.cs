ccea2fd84015511d73d0b0860701f4d5fe2375d1
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-07-11 00:01:49 +0300

    1st new programm n*n

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 0. Демонстрация решения
+
+Напишите программу, которая на вход
+принимает число и выдаёт его квадрат (число
+умноженное на само себя). */
+
+/* int n=10;
+Console.WriteLine("Число: " +n);
+Console.WriteLine("Квадрат: " +n*n); */
+
+Console.WriteLine("Введите число: ");
+int n=int.Parse(Console.ReadLine());
+Console.WriteLine("Число: " +n);
+Console.WriteLine("Квадрат: " +n*n);
+
+/* int A=10;
+int i=0;
+while (i<=A)
+{
+    Console.WriteLine(i);
+    i++;
+} */
