04b351895bf7041a1ed9ad77f5c7edd927226498
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-07-17 21:35:26 +0300

    homework finish

--- HomeWork_Lesson1/Program.cs
+++ HomeWork_Lesson1/Program.cs
@@ -84,3 +84,10 @@ else
 числа от 1 до N.
 5 -> 2, 4
 8 -> 2, 4, 6, 8 */
+
+Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("N = " + a);
+for (int N = 1; N <= a; N++)
+    if (N % 2 == 0)
+        Console.WriteLine(N);
